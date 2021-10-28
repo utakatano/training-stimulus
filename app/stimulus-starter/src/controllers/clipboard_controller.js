65e44f644837e4abfdffa2027a0c9862c41279f9
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-10-28 23:59:40 +0900

    styling

--- app/stimulus-starter/src/controllers/clipboard_controller.js
+++ app/stimulus-starter/src/controllers/clipboard_controller.js
@@ -2,6 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = [ 'source' ]
+  static classes = [ "supported" ]
 
   copy(event) {
     event.preventDefault()
