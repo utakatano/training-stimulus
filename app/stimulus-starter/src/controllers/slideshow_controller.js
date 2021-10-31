26ae15ce089b1edd7627e78efb0ff983d0d1f599
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-10-31 23:38:16 +0900

    update-slideshow

--- app/stimulus-starter/src/controllers/slideshow_controller.js
+++ app/stimulus-starter/src/controllers/slideshow_controller.js
@@ -2,25 +2,27 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = [ 'slide' ]
+  static values = { index: Number }
 
   initialize() {
-    this.index = 0
     this.showCurrentSlide()
   }
 
   next() {
-    this.index++
-    this.showCurrentSlide()
+    this.indexValue++
   }
 
   previous() {
-    this.index--
+    this.indexValue--
+  }
+
+  indexValueChanged() {
     this.showCurrentSlide()
   }
 
   showCurrentSlide() {
     this.slideTargets.forEach((element, index) => {
-      element.hidden = index != this.index
+      element.hidden = index != this.indexValue
     })
   }
 }
