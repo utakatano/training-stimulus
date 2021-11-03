18e77b2dd1652c49d0f243c61354fced1846d35c
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-11-03 23:49:50 +0900

    reflesh-with-timer

--- app/stimulus-starter/src/controllers/content_loader_controller.js
+++ app/stimulus-starter/src/controllers/content_loader_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static values = { url: String }
+  static values = { url: String, refleshInterval: Number }
 
   connect() {
     this.load()
+
+    if (this.hasRefreshIntervalValue) {
+      this.startRefreshing()
+    }
+  }
+
+  startRefreshing() {
+    setInterval(() => {
+      this.load()
+    }, this.refleshIntervalValue)
   }
 
   load() {
